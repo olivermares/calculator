58fe3336611cff37d8762071d4b05a83f6e5a11f
olivermares <olivermares@hotmail.com>
2023-07-30 11:41:57 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <div>
-      <h2>Esto es el Footer</h2>
-    </div>
+    <>
+    <div className="footer-links"></div>
+      <h2 className="footer-title">Esto es el Footer</h2>
+      <div className="footer-social"></div>
+    </>
   );
 }
