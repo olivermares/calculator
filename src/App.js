a52577f2543b43844e678b94dae865eff2fad061
olivermares <olivermares@hotmail.com>
2023-07-30 11:49:57 +0200

    main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main className="main"/>
       <Footer />
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,9 @@
 export default function Main() {
   return (
-    <div>
-      <h2>Esto es el main</h2>
-    </div>
+    <>
+      <h2 className="main-title">Esto es el main</h2>
+      <div className="calculator">
+      </div>
+    </>
   );
 }
